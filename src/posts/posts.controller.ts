2edf0a8e57e142287773eaa90d403502c3cb353f
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-11-06 11:08:44 -0600

    feat: implemented to document about api

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -20,21 +20,28 @@ export class PostsController {
     }
 
     @Get(':id')
+    @ApiOperation({summary:'Get Post By Id'})
+    @ApiResponse({status:200, description:'Return Post'})
+    @ApiResponse({status:404,description:'Not Found'})
+    @ApiResponse({status:400,description:'Forbidden'})
     async getPostById(@Param('id',ParseIntPipe) id:number){
         return await this.postsService.getPostById(id)
     }
 
     @Post()
+    @ApiOperation({summary:'Create Post'})
     async createPost(@Body() post:CreatePostDto){
         return await this.postsService.createPost(post)
     }
     
     @Put(':id')
+    @ApiOperation({summary:'Update Post'})
     async updatePost(@Param('id',ParseIntPipe) id:number, @Body() post:UpdatePostDto){
         return await this.postsService.updatePost(id,post)
     }
 
     @Delete(':id')
+    @ApiOperation({summary:'Delete Post'})
     async deletePost(@Param('id',ParseIntPipe) id:number){
         return await this.postsService.deletePost(id)
     }
