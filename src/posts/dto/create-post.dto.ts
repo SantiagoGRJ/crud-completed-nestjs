6e820e1b3a1e5f3660193c912ad3fdf02961b15e
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-11-07 09:25:39 -0600

    feat: add more decorators of swagger

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,12 +1,23 @@
+import { ApiProperty, ApiPropertyOptional } from "@nestjs/swagger"
 import { IsNotEmpty, IsNumber, IsString } from "class-validator"
 
 export class CreatePostDto {
 
+    @ApiProperty({
+        description:'The title of a Post',
+        minLength:2,
+        maxLength:20
+    })
+
     @IsString()
     name:string
 
+    @ApiPropertyOptional()
+
     @IsString()
     content:string
+
+    @ApiProperty()
 
     @IsNotEmpty()
     @IsNumber()
--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,14 +1,21 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsNotEmpty, IsNumber, IsString } from "class-validator"
 
 export class UpdatePostDto{
+
+    @ApiProperty()
     
     @IsNotEmpty()
     @IsString()
     name:string
     
+    @ApiProperty()
+    
     @IsNotEmpty()
     @IsString()
     content:string
+    
+    @ApiProperty()
 
     @IsNotEmpty()
     @IsNumber()
