35e0dc67cd3ee34b01d81ebc5084bbd9e229e0e2
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-11-08 10:57:10 -0600

    fix: remove comments

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -13,11 +13,7 @@ export class PostsController {
 
     constructor(private postsService:PostsService){}
 
-    @Get()
-    @ApiOperation({summary:'Get All Posts'})
-    @ApiResponse({status:200, description:'Return all Posts'})
-    @ApiResponse({status:403, description:'Forbidden.'})
-    
+    @Get()    
     async getAllPosts(
         @Query('name') name:string,
         @Query('content') content:string,
@@ -28,28 +24,21 @@ export class PostsController {
     }   
 
     @Get(':id')
-    @ApiOperation({summary:'Get Post By Id'})
-    @ApiResponse({status:200, description:'Return Post'})
-    @ApiResponse({status:404,description:'Not Found'})
-    @ApiResponse({status:400,description:'Forbidden'})
     async getPostById(@Param('id',ParseIntPipe) id:number){
         return await this.postsService.getPostById(id)
     }
 
     @Post()
-    @ApiOperation({summary:'Create Post'})
     async createPost(@Body() post:CreatePostDto){
         return await this.postsService.createPost(post)
     }
     
     @Put(':id')
-    @ApiOperation({summary:'Update Post'})
     async updatePost(@Param('id',ParseIntPipe) id:number, @Body() post:UpdatePostDto){
         return await this.postsService.updatePost(id,post)
     }
 
     @Delete(':id')
-    @ApiOperation({summary:'Delete Post'})
     async deletePost(@Param('id',ParseIntPipe) id:number){
         return await this.postsService.deletePost(id)
     }
