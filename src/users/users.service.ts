7d4b8545a94aeb49280704aeffcb79752ed439c4
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-11-19 10:23:13 -0600

    feat: implement create, and getUserByEmail methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,40 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "../prisma.service";
+import { NotFoundError } from "rxjs";
+import { IUser } from "./interfaces/user.interface";
 
 @Injectable()
 export class UsersService {
 
-    constructor(private prisma:PrismaService) {}
+    constructor(private prisma: PrismaService) { }
 
-    async getUserByEmail(email:string) {
-
+    async createUser(data:IUser) {
+        try {
+            const user = await this.prisma.user.create({data})
+            return user
+        } catch (err) {
+            if(err?.code ==="P2002"){
+                throw new ConflictException(`The ${err?.meta} is taken, try another different`)
+            }
+            throw new InternalServerErrorException('Create User Error')
+        }
     }
+
+    async getUserByEmail(email: string) {
+        try {
+
+            const user = await this.prisma.user.findUnique({
+                where: {
+                    email: email
+                }
+            })
+            return user
+        } catch (err) {
+            if (err?.code === "P2025") {
+                throw new NotFoundException(`User not found, try again`)
+            }
+            throw new InternalServerErrorException('Searching method Error', err)
+        }
+    }
+
 }
