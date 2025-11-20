d84c7f5ebec2b6b30e99cdfe599d482d3c879680
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-11-20 16:50:19 -0600

    fix: solution project

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { UsersModule } from "../users/users.module";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
-import { JWT_CONSTANTS } from "./constants";
+import { JWT_CONSTANTS } from "../config/constants";
 
 
 @Module({
--- src/auth/constants.ts
+++
@@ -1,6 +0,0 @@
-
-
-export const JWT_CONSTANTS = {
-
-    secret:"SE CAMBIO POR FALLO EN LA SUB DE ARCH"
-}
