cb8f0a68dbe6127ae83b1c129f355de3fbcddc6c
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-11-21 13:44:47 -0600

    feat: add createRole v0.1

---
+++ src/roles/roles.service.ts
@@ -0,0 +1,21 @@
+import { ConflictException, Injectable, InternalServerErrorException } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+
+@Injectable()
+export class RolesService {
+
+    constructor(private prismaService: PrismaService) { }
+
+    async createRole(input: { name: string }) {
+        try {
+            const role = await this.prismaService.role.create({ data: input })
+
+            return role
+        } catch (err) {
+            if (err?.code == "P2002") {
+                throw new ConflictException(`The ${err.meta?.target} is taken, try another different`)
+            }
+            throw new InternalServerErrorException()
+        }
+    }
+}
