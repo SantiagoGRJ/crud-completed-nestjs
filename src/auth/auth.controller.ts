3e932e3573ba36b075475191a3829dcc3116bbe6
SantiagoGRJ <santiago-2003-rojas@hotmail.com>
2025-11-20 18:13:54 -0600

    fix: add Authguard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Request, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { AuthGuard } from "./guard/auth.guard";
 
 @Controller('auth')
 export class AuthController {
@@ -15,6 +16,12 @@ export class AuthController {
     @Post('login')
     async logIn(@Body() user:{email:string,password:string}){
         return await this.authService.authenticate(user)
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    getInfoProfile(@Request() request){
+        return request.user
     }
 
 }
---
+++ src/auth/guard/auth.guard.ts
@@ -0,0 +1,35 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+    constructor(private jwtService: JwtService) { }
+
+    async canActivate(context: ExecutionContext) {
+        const request = context.switchToHttp().getRequest() as Request
+        const token = this.extractTokenFromHeader(request)
+
+        if (!token) {
+            throw new UnauthorizedException()
+        }
+
+        try {
+            const tokenPayload = await this.jwtService.verifyAsync(token)
+            request['user'] = tokenPayload
+            return true
+        } catch (error) {
+            throw new UnauthorizedException()
+
+        }
+
+    }
+
+    extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(' ') ?? []
+
+        return type === "Bearer" ? token : undefined
+    }
+}
